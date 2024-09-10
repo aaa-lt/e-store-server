81a636a68943855fa8e162a4b9f4efaee860199c
Ealten <1jonasmadsenfake1@gmail.com>
2024-09-10 19:07:44 +0300

    update: seeder optimize

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ const syncDatabase = async () => {
         await sequelize.authenticate();
         console.log("Connection has been established successfully.");
         await sequelize.sync();
-        // await sequelize.sync({ alter: true });
         console.log("Database synchronized.");
     } catch (error) {
         console.error("Unable to connect to the database:", error);
--- src/seeder.js
+++ src/seeder.js
@@ -3,16 +3,40 @@ import sequelize from "../config/db.js";
 import Category from "./models/Category.js";
 import Supplier from "./models/Supplier.js";
 import Product from "./models/Product.js";
+import OrderProduct from "./models/OrderProduct.js";
+import Order from "./models/Order.js";
+import User from "./models/User.js";
+
+const syncDatabase = async () => {
+    try {
+        await sequelize.authenticate({ logging: false });
+        console.log("Connection has been established successfully.");
+        await sequelize.sync({ force: true, logging: false });
+        console.log("Database synchronized.");
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
+};
 
 async function seedDatabase() {
     try {
-        await sequelize.authenticate();
-        await sequelize.sync();
+        console.time("seedDatabase");
+        const transaction = await sequelize.transaction();
+        console.time("syncDB");
+        await syncDatabase();
+
+        console.timeEnd("syncDB");
+        console.time("seedCategories");
 
         const categories = Array.from({ length: 10000 }).map((_, index) => ({
             name: `${faker.commerce.department()}${index + 1}`,
             description: faker.lorem.sentence(),
         }));
+
+        await Category.bulkCreate(categories, { logging: false });
+
+        console.timeEnd("seedCategories");
+        console.time("seedSuppliers");
 
         const suppliers = Array.from({ length: 10000 }).map((_, index) => ({
             name: faker.company.name(),
@@ -20,12 +44,15 @@ async function seedDatabase() {
             phone_number: faker.phone.number({ style: "international" }),
         }));
 
-        await Category.bulkCreate(categories);
-        await Supplier.bulkCreate(suppliers);
+        await Supplier.bulkCreate(suppliers, { logging: false });
 
-        const categoryRecords = await Category.findAll();
-        const supplierRecords = await Supplier.findAll();
+        console.timeEnd("seedSuppliers");
+        console.time("seedProducts");
 
+        const [categoryRecords, supplierRecords] = await Promise.all([
+            Category.findAll({ logging: false }),
+            Supplier.findAll({ logging: false }),
+        ]);
         const products = Array.from({ length: 200000 }).map(() => ({
             name: faker.commerce.productName(),
             description: faker.commerce.productDescription(),
@@ -35,13 +62,21 @@ async function seedDatabase() {
             supplier_id: faker.helpers.arrayElement(supplierRecords).id,
         }));
 
-        await Product.bulkCreate(products);
+        const BATCH_SIZE = 5000;
+        for (let i = 0; i < products.length; i += BATCH_SIZE) {
+            const productBatch = products.slice(i, i + BATCH_SIZE);
+            await Product.bulkCreate(productBatch, { logging: false });
+        }
 
+        await transaction.commit();
+        console.timeEnd("seedProducts");
         console.log("Database seeded successfully");
     } catch (error) {
+        await transaction.rollback();
         console.error("Error seeding database:", error);
     } finally {
         await sequelize.close();
+        console.timeEnd("seedDatabase");
     }
 }
 
